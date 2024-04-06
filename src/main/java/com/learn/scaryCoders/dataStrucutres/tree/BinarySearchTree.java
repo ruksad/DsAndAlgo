2c422656ea82f8f231decfc9d0ee7673022cb7da
ruksad <sidsid2810@gmail.com>
2024-04-06 16:56:24 +0530

    BST inorder, preorder, postorder traversal

--- src/main/java/com/learn/scaryCoders/dataStrucutres/tree/BinarySearchTree.java
+++ src/main/java/com/learn/scaryCoders/dataStrucutres/tree/BinarySearchTree.java
@@ -1,7 +1,14 @@
 package com.learn.scaryCoders.dataStrucutres.tree;
 
-import com.learn.scaryCoders.dataStrucutres.DoublyLinkedList;
+import javax.sound.sampled.Line;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
+/**
+ * Left of the root is smaller than the root and right of the root is greater
+ */
 public class BinarySearchTree {
 
     private Node root;
@@ -16,14 +23,20 @@ public class BinarySearchTree {
             return root;
         }
         Node temp = root;
+
         while (true) {
-            if (data > temp.data) {
-                temp=temp.left;
+            if (data < temp.data) {
                 if(temp.left==null){
                     temp.left= new Node(data,null,null);
+                    return temp.left;
                 }
+                temp=temp.left;
             }else{
-
+                if(temp.right==null){
+                    temp.right= new Node(data,null,null);
+                    return temp.right;
+                }
+                temp=temp.right;
             }
         }
     }
@@ -38,5 +51,60 @@ public class BinarySearchTree {
             this.left = left;
             this.right = right;
         }
+    }
+
+    private int findMin(){
+        Node temp= root;
+        while(temp.left!=null){
+            temp=temp.left;
+        }
+        return temp.data;
+    }
+
+    private void inOrderTraversal(Node root, LinkedList<Integer> list){
+        if(root !=null){
+            inOrderTraversal(root.left,list);
+            list.add(root.data);
+            inOrderTraversal(root.right,list);
+        }
+    }
+
+    private void preOrderTraversal(Node root, LinkedList<Integer> list){
+        if(root!=null){
+            list.add(root.data);
+            preOrderTraversal(root.left,list);
+            preOrderTraversal(root.right,list);
+        }
+    }
+
+    private void postOrderTraversal(Node root, LinkedList<Integer> list){
+        if(root!=null){
+            postOrderTraversal(root.left, list);
+            postOrderTraversal(root.right,list);
+            list.add(root.data);
+        }
+    }
+    public static void main(String[] args) {
+
+        BinarySearchTree bst= new BinarySearchTree();
+        bst.insert(40);
+        bst.insert(50);
+        bst.insert(20);
+        bst.insert(30);
+        bst.insert(25);
+        bst.insert(45);
+        bst.insert(46);
+        System.out.println(bst.findMin());
+        LinkedList<Integer> inOrder = new LinkedList<>();
+        bst.inOrderTraversal(bst.root, inOrder);
+        System.out.println("Inorder= "+inOrder);
+
+        LinkedList<Integer> preOrder = new LinkedList<>();
+        bst.preOrderTraversal(bst.root, preOrder);
+        System.out.println("Preorder= "+preOrder);
+
+        LinkedList<Integer> postOrder = new LinkedList<>();
+        bst.postOrderTraversal(bst.root, postOrder);
+        System.out.println("PostOrder= "+postOrder);
     }
 }

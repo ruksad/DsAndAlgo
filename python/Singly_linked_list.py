1e863ac84fd986bd95eabdf102916bcff10d756d
ruksad <sidsid2810@gmail.com>
2024-04-08 19:33:42 +0530

    refactoring

--- python/Singly_linked_list.py
+++ python/Singly_linked_list.py
@@ -1,56 +1,56 @@
 class Node:
-      def __init__(self,data):
-          self.data=data
-          self.next= None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 
 class LinkedList:
-     def __init__(self):
-          self.size=0
-          self.head= None
-
-def addNode(linked_list, data):
-     if(linked_list==None):
-          print("linkedList is not initialized")
-        
-     node= Node(data)
-     node.next= None
-     
-     temp= linked_list.head
-     
-     if(temp==None):
-        linked_list.head= node
-     else:
-        while(temp.next!=None):
-            temp=temp.next
-
-        temp.next= node   
-
-     linked_list.size+=1
-     
-
-def printLinkedList(linked_list):
-     if isEmpty(linked_list):
-          print("Empty linked list")
-    
-     temp=linked_list.head
-     while temp.next!=None:
-          print(temp.data, end=",")
-          temp=temp.next
-     print(temp.data)
-    
-    
-def isEmpty(linked_list):
-     return (linked_list.size==0 or linked_list==None)
-
-if __name__=="__main__":
-     linked_list= LinkedList()
-
-     addNode(linked_list, str("Ruksad"))
-     addNode(linked_list, str("Ruksad1"))
-     addNode(linked_list, str("Siddiqui11"))
-     addNode(linked_list, str("Siddiqui"))
-     addNode(linked_list, str("Mohammad1"))
-     printLinkedList(linked_list)
+    def __init__(self):
+        self.size = 0
+        self.head = None
 
 
+def add_node(linked_list, data):
+    if linked_list is None:
+        print("linkedList is not initialized")
+
+    node = Node(data)
+    node.next = None
+
+    temp = linked_list.head
+
+    if temp is None:
+        linked_list.head = node
+    else:
+        while temp.next is not None:
+            temp = temp.next
+
+        temp.next = node
+
+    linked_list.size += 1
+
+
+def print_linked_list(linked_list):
+    if is_empty(linked_list):
+        print("Empty linked list")
+
+    temp = linked_list.head
+    while temp.next is not None:
+        print(temp.data, end=",")
+        temp = temp.next
+    print(temp.data)
+
+
+def is_empty(linked_list):
+    return linked_list.size == 0 or linked_list is None
+
+
+if __name__ == "__main__":
+    linked_list = LinkedList()
+
+    add_node(linked_list, str("Ruksad"))
+    add_node(linked_list, str("Ruksad1"))
+    add_node(linked_list, str("Siddiqui11"))
+    add_node(linked_list, str("Siddiqui"))
+    add_node(linked_list, str("Mohammad1"))
+    print_linked_list(linked_list)
--- python/doubly_linked_list.py
+++ python/doubly_linked_list.py
@@ -1,80 +1,80 @@
 class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-        self.prev=None
-    
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
 
 class Doubly_linked_list:
     def __init__(self):
-        self.size=0
-        self.head=None
-        self.tail=None
+        self.size = 0
+        self.head = None
+        self.tail = None
 
-def printDoublyLinkedList(linked_list):
 
-    if isNone(linked_list) or isEmpty(linked_list):
+def print_doubly_linked_list(linked_list):
+    if is_none(linked_list) or is_empty(linked_list):
         print("Please pass a non empty linked list")
         return
 
-    temp= linked_list.head
+    temp = linked_list.head
 
-    while temp.next!=None :
-        print(temp.data,end=",")
-        temp=temp.next
+    while temp.next is not None:
+        print(temp.data, end=",")
+        temp = temp.next
     print(temp.data)
 
 
-def addNodeAt(linked_list, data,location):
-    node =Node(str(data))
-    if isEmpty(linked_list):
-        linked_list.head=node
-        linked_list.tail=node
-        linked_list.size+=1
+def add_note_at(linked_list, data, location):
+    node = Node(str(data))
+    if is_empty(linked_list):
+        linked_list.head = node
+        linked_list.tail = node
+        linked_list.size += 1
     else:
-        temp= linked_list.head
-        if location<=0:
-            location=1
-        elif location> linked_list.size:
-            location= linked_list.size
+        temp = linked_list.head
+        if location <= 0:
+            location = 1
+        elif location > linked_list.size:
+            location = linked_list.size
 
-        for i in range(1,location+1):
+        for i in range(1, location + 1):
 
             if i == location:
-                if location==1:
-                    node.next=linked_list.head
-                    linked_list.head.prev=node
-                    linked_list.head= node                 
-                elif location==linked_list.size:
-                    node.prev=linked_list.tail
-                    linked_list.tail.next=node
-                    linked_list.tail=node
+                if location == 1:
+                    node.next = linked_list.head
+                    linked_list.head.prev = node
+                    linked_list.head = node
+                elif location == linked_list.size:
+                    node.prev = linked_list.tail
+                    linked_list.tail.next = node
+                    linked_list.tail = node
                 else:
-                    node.next=temp
-                    node.prev=temp.prev
-                    temp.prev.next=node
-                    temp.prev=node
-                linked_list.size+=1
-            temp=temp.next
+                    node.next = temp
+                    node.prev = temp.prev
+                    temp.prev.next = node
+                    temp.prev = node
+                linked_list.size += 1
+            temp = temp.next
 
 
-def isNone(linked_list):
-    return linked_list==None
+def is_none(linked_list):
+    return linked_list == None
 
-def isEmpty(linked_list):
-    return linked_list.size==0
+
+def is_empty(linked_list):
+    return linked_list.size == 0
 
 
 if __name__ == "__main__":
-
-    dll= Doubly_linked_list()
-    addNodeAt(dll,"Mohammad",-1)
-    addNodeAt(dll,"Ruksad",-1)
-    addNodeAt(dll,"derric",100)
-    addNodeAt(dll,"Siddiqui",-10)
-    addNodeAt(dll,"samuel",2)
-    printDoublyLinkedList(dll)
-    addNodeAt(dll,"samuel1",2)
-    printDoublyLinkedList(dll)
-    addNodeAt(dll,"samuel2",3)
-    printDoublyLinkedList(dll)
+    dll = Doubly_linked_list()
+    add_note_at(dll, "Mohammad", -1)
+    add_note_at(dll, "Ruksad", -1)
+    add_note_at(dll, "derric", 100)
+    add_note_at(dll, "Siddiqui", -10)
+    add_note_at(dll, "samuel", 2)
+    print_doubly_linked_list(dll)
+    add_note_at(dll, "samuel1", 2)
+    print_doubly_linked_list(dll)
+    add_note_at(dll, "samuel2", 3)
+    print_doubly_linked_list(dll)

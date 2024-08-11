e4e9b890d7141d8454d7ea47a16f3ba7330898fe
ruksad <sidsid2810@gmail.com>
2024-08-11 11:47:30 +0530

    insertion sort

--- src/main/java/com/learn/scaryCoders/logical/arrays/Sorting.java
+++ src/main/java/com/learn/scaryCoders/logical/arrays/Sorting.java
@@ -8,7 +8,7 @@ public class Sorting {
         int[] array = {23, 20, 19, 10, 2, 2, -1, -5, -100};
 
         long startTime = System.currentTimeMillis();
-        int[] ints = selectionSort(array);
+        int[] ints = insertionSort(array);
         long endTime = System.currentTimeMillis();
         System.out.println("Time taken by sorting="+(endTime-startTime)+" Sorted array="+Arrays.toString(ints));
 
@@ -16,6 +16,15 @@ public class Sorting {
 
 
     //Comparing the adjacent element
+
+    /**
+     * In Bubble Sort algorithm     *
+     * traverse from left and compare adjacent elements and the higher one is placed at right side.
+     * In this way, the largest element is moved to the rightmost end at first.
+     * This process is then continued to find the second largest and place it and so on until the data is sorted.
+     * @param array
+     * @return
+     */
     public static int[] bubbleSort(int[] array) {
 
         for (int i = 0; i < array.length - 1; i++) {
@@ -31,13 +40,21 @@ public class Sorting {
     }
 
     // Placing the smallest element at start
+
+    /**
+     * Selection sort is a simple and efficient sorting algorithm that works by repeatedly
+     * selecting the smallest (or largest) element from the unsorted portion of the list and moving
+     * it to the sorted portion of the list.
+     * @param array
+     * @return
+     */
     public static int[] selectionSort(int[] array) {
 
         for (int i = 0; i < array.length - 1; i++) {
             int pos = i;
 
             for (int j = i + 1; j < array.length; j++) {
-                if (array[pos] > array[j]) {  // check if first element is bigger than then next in the arrya
+                if (array[j] < array[pos]) {  // check if first element is bigger than then next in the arrya
                     pos = j;
                 }
             }
@@ -51,5 +68,29 @@ public class Sorting {
         return array;
     }
 
+
+    /**
+     *
+     * Insertion sort is a simple sorting algorithm that works by iteratively inserting each element of an unsorted list into its correct position
+     * in a sorted portion of the list.
+     * It is a stable sorting algorithm, meaning that elements with equal values maintain their relative order in the sorted output.
+     * https://www.geeksforgeeks.org/insertion-sort-algorithm/
+     * @param array  {23, 20, 19, 10, 2, 2, -1, -5, -100};
+     * @return
+     */
+    public static int[] insertionSort(int [] array){
+        for(int i=1;i<array.length;i++){
+            int temp=array[i];
+
+            for(int j=i-1;j>=0;j--){
+                if(temp<array[j]){
+                    array[j+1]=array[j];
+                }
+                array[j]=temp;
+            }
+
+        }
+        return array;
+    }
 
 }
